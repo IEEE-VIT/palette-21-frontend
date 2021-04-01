adc3dad09eca5861dc417b91675b459a323a0982
Agniva Basak <agnivabasak1@gmail.com>
2021-04-01 21:47:15 +0530

    Set File Structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import LandingScreen from "./screens/LandingScreen/LandingScreen";
+import LoginScreen from "./screens/LoginScreen/LoginScreen";
+import UserFormScreen from "./screens/UserFormScreen/UserFormScreen";
 
-function App() {
+const AppRouter = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={LandingScreen} />
+          <Route exact path="/login" component={LoginScreen} />
+          <Route exact path="/userForm" component={UserFormScreen} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
-export default App;
+export default AppRouter;
---
+++ src/screens/LandingScreen/LandingScreen.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./LandingScreen.css";
+
+export default function LandingScreen() {
+  return <div>Hello, Welcome to palette !</div>;
+}

8d2e5d45296dc7f31cb398a1c40b2014fc945f51
souris-dev <suorees113@gmail.com>
2021-05-05 17:08:43 +0530

    Made things responsive

--- src/components/LandingPage/Event/EventBanner.jsx
+++ src/components/LandingPage/Event/EventBanner.jsx
@@ -18,13 +18,13 @@ function EventBanner(props) {
 
   return (
     <div className="container-fluid bg-bg whiteb">
-      <div className="big-screen">
+      <div className="d-none d-md-none d-xs-none d-sm-none d-lg-block">
         <div className="img-arr d-flex">
           <img src={spikedCircleTl} className="img-arr-img-tl" />
           <img src={judgesText} className="img-arr-judges-topr" />
           <div className="flex-grow-1">
             <div className="d-flex flex-row">
-              <div className="col-md-6 flex-grow-1 d-flex flex-column justify-content-between z-index-mid">
+              <div className="col-sm-6 col-md-6 flex-grow-1 d-flex flex-column justify-content-between z-index-mid">
                 <div className="ml-10rem pt-12rem">
                   <span className="red-allcaps">{eventCategory}</span>
                   <p className="mt-2">
@@ -50,7 +50,7 @@ function EventBanner(props) {
                   </h5>
                 </div>
               </div>
-              <div className="col-md-6 flex-grow-1">
+              <div className="col-sm-6 col-md-6 flex-grow-1">
                 <div>
                   <img src={speakersTextBg} className="speakers-text" />
                   <img src={speakerPic} className="speaker-pic" />
@@ -68,48 +68,48 @@ function EventBanner(props) {
           </div>
         </div>
       </div>
-      <div className="small-screen">
-        {/*} <div className="flex-grow-1">
+      <div className="d-lg-none">
+        <div className="flex-grow-1 img-arr-sm">
           <div className="d-flex flex-column">
-            <div className="col-md-6 flex-grow-1">
-              <div>
-                <img src={speakersTextBg} className="speakers-text" />
-                <img src={speakerPic} className="speaker-pic" />
-              </div>
-              <div className="z-top-text">
-                <h1 className="white-text-bold">
-                  <b>{speakerName}</b>
-                </h1>
-                <h5 style={{ fontWeight: 300 }} className="mt-1 mr-2 line-sp">
-                  {speakerDesc}
-                </h5>
-              </div>
+            <div>
+              <img src={judgesText} className="img-arr-judges-topr-sm" />
+              <img src={speakersTextBg} className="speakers-text-sm" />
+              <img src={speakerPic} className="speaker-pic-small" />
             </div>
-            <div className="col-md-6 flex-grow-1 d-flex flex-column justify-content-between z-index-mid">
-              <div className="ml-10rem pt-12rem">
-                <span className="red-allcaps">{eventCategory}</span>
-                <p className="mt-2">
-                  <h1>
-                    <i>
-                      <b>{eventName}</b>
-                    </i>
-                  </h1>
-                </p>
-                <h5 style={{ fontWeight: 300 }} className="mt-4p5 mr-2 line-sp">
-                  {eventDesc}
-                </h5>
-              </div>
-              <div className="ml-10rem pt-20rem mb-5r">
-                <h1 className="red-text-bold">
-                  <b>{date}</b>
-                </h1>
-                <h5 style={{ fontWeight: 300 }} className="mt-1 mr-2 line-sp">
-                  Hosted on
-                </h5>
-              </div>
+            <div className="speaker-desc-sm">
+              <h1 className="white-text-bold">
+                <b>{speakerName}</b>
+              </h1>
+              <h5 style={{ fontWeight: 300 }} className="mt-1 mr-2 line-sp">
+                {speakerDesc}
+              </h5>
             </div>
           </div>
-        </div>*/}
+          <div className="event-block">
+            {/*}> className="flex-grow-1 d-flex flex-column justify-content-between z-index-mid">*/}
+            <div className="pt-5">
+              <span className="red-allcaps">{eventCategory}</span>
+              <p className="mt-2">
+                <h1>
+                  <i>
+                    <b>{eventName}</b>
+                  </i>
+                </h1>
+              </p>
+              <h5 style={{ fontWeight: 300 }} className="mt-4p5 mr-2 line-sp">
+                {eventDesc}
+              </h5>
+            </div>
+            <div className="pt-30vh mb-5r">
+              <h1 className="red-text-bold">
+                <b>{date}</b>
+              </h1>
+              <h5 style={{ fontWeight: 300 }} className="mt-1 mr-2 line-sp">
+                Hosted on
+              </h5>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );

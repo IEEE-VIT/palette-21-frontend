30b9d58473fb3407a95b750059e201f840fb328b
souris-dev <suorees113@gmail.com>
2021-05-05 08:11:40 +0530

    Made section viewport size

--- src/components/LandingPage/EventThemes/EventThemes.jsx
+++ src/components/LandingPage/EventThemes/EventThemes.jsx
@@ -36,48 +36,41 @@ function EventThemes(props) {
 
   return (
     <div className="container-fluid headcont whiteb">
-      <div className="img-arr">
+      <div className="img-arr d-flex">
         <img src={spikedCircleTr} className="img-arr-img-tr" />
         <img src={spikedCircleBl} className="img-arr-img-bl" />
-        <div className="row">
+        <div className="row flex-grow-1">
           <div className="col-sm-6 col-md-6"></div>
-          <div className="col-sm-6 col-md-6">
-            <div className="mt-5r mr-5 ml-4">
-              <div>
-                <h1>
-                  <i>Introducing our theme</i>
-                </h1>
-                <h5
-                  style={{ fontWeight: 300 }}
-                  className="mt-3 mr-10rm line-sp"
-                >
-                  Convallis turpis ellat tempus, viverra aliquet. Nul lam num
-                  aucter sit ipsum malesuada a, duis volutpat.
-                </h5>
+          <div className="col-sm-6 col-md-6 d-flex flex-column justify-content-around pl-5 pr-5 overflow-wrp">
+            <div className="pt-8rem flex-grow-1 d-flex flex-column justify-content-center">
+              <h1 className="mr-5r">
+                <i>Introducing our theme</i>
+              </h1>
+              <h5 style={{ fontWeight: 300 }} className="mt-3 mr-10rm line-sp">
+                Convallis turpis ellat tempus, viverra aliquet. Nul lam num
+                aucter sit ipsum malesuada a, duis volutpat.
+              </h5>
+            </div>
+            <div className="pt-8rem d-flex flex-column pb-5r flex-grow-1">
+              <div className="flex-grow-1">
+                <h1 className="big-head">{eventThemeTitles[selectedIndex]}</h1>
+                <h4 className="big-head-subt mt-3 flex-grow-1">
+                  {eventThemeDescr[selectedIndex]}
+                </h4>
               </div>
-              <div className="pb-5r">
-                <div className="mt-5rbig">
-                  <h1 className="big-head">
-                    {eventThemeTitles[selectedIndex]}
-                  </h1>
-                  <h4 className="big-head-subt mt-3">
-                    {eventThemeDescr[selectedIndex]}
-                  </h4>
-                </div>
-                <div className="mt-2p6">
-                  <div className="d-flex flex-row mb-3">
-                    {eventThemeTitles.map((elem, idx) => {
-                      var isSelected = selectedIndex === idx;
-                      return (
-                        <SelectorButton
-                          text={elem}
-                          key={idx}
-                          selected={isSelected}
-                          onClick={() => setSelectedIndex(idx)}
-                        />
-                      );
-                    })}
-                  </div>
+              <div className="mt-2p6">
+                <div className="d-flex flex-row flex-wrap mb-3">
+                  {eventThemeTitles.map((elem, idx) => {
+                    var isSelected = selectedIndex === idx;
+                    return (
+                      <SelectorButton
+                        text={elem}
+                        key={idx}
+                        selected={isSelected}
+                        onClick={() => setSelectedIndex(idx)}
+                      />
+                    );
+                  })}
                 </div>
               </div>
             </div>

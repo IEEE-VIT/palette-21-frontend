0c921c1c69507c653e0d3f24b13d54839d10ce13
Crazy-C0der <sh2000.cool.j@gmail.com>
2021-05-06 23:50:40 +0530

    Responsive Navbar

--- src/components/LandingPage/Navbar/Navbar.jsx
+++ src/components/LandingPage/Navbar/Navbar.jsx
@@ -1,85 +1,139 @@
-/* eslint-disable prettier/prettier */
 /* eslint-disable react/prop-types */
 import React from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import { makeStyles } from "@material-ui/core";
-import { useScrollTrigger } from "@material-ui/core";
-import { ThemeProvider, CssBaseline } from "@material-ui/core";
-import { createMuiTheme } from "@material-ui/core";
+import Logo from "../../../assets/palette-nav.svg";
+import { Nav, Navbar } from "react-bootstrap";
 
 import "./Navbar.css";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-    colour: "black"
-  },
-  appBarTransparent: {
-    backgroundColor: "rgba(255, 255, 255, 0)"
-  },
-  appBarSolid: {
-    backgroundColor: "rgba(255, 255, 255, 1)"
+export default class NavbarComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.listener = null;
+    this.state = {
+      status: "top",
+      show: false,
+    };
   }
-}));
 
-const ScrollHandler = (props) => {
-  const trigger = useScrollTrigger({
-    disableHysteresis: true,
-    threshold: screen.availHeight * 0.9,
-    target: props.window ? window() : undefined
-  });
+  componentDidMount() {
+    this.listener = document.addEventListener("scroll", () => {
+      if (window.scrollY > screen.availHeight * 0.9) {
+        this.setState({ status: "scrolled", show: true });
+      } else {
+        if (this.state.status !== "top") {
+          this.setState({ status: "top", show: false });
+        }
+      }
+    });
+  }
 
-  return React.cloneElement(props.children, {
-    style: {
-      backgroundColor: trigger ? "rgba(255, 255, 255, 1)" : "transparent",
-      color: trigger ? "black" : "black",
-      transition: trigger ? "0.3s" : "0.3s",
-      boxShadow: trigger ? "0 4px 14px 0 rgba(130, 130, 130, 0.4)" : "none",
-      padding: "2px 0px",
-      position: "fixed"
-    }
-  });
-};
+  componentDidUpdate() {
+    document.removeEventListener("scroll", this.listener);
+  }
 
-const ScrollToColor01 = (props) => {
-  return <ScrollHandler {...props}>{props.children}</ScrollHandler>;
-};
-
-const Navbar01scrollToColor = (props) => {
-  const theme = createMuiTheme();
-
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-
-      <ScrollToColor01>
-        <AppBar position="static">{props.children}</AppBar>
-      </ScrollToColor01>
-    </ThemeProvider>
-  );
-};
-
-export default function Navbar() {
-  const classes = useStyles();
-  return (
-    <div>
-      <Navbar01scrollToColor>
-        <Toolbar>
-          <IconButton className={classes.menuButton}>
-            <MenuIcon />
-          </IconButton>
-          <Button variant="outlined" color="secondary">
-            Register
-          </Button>
-        </Toolbar>
-      </Navbar01scrollToColor>
-    </div>
-  );
+  render() {
+    var title = "Palette'21";
+    return (
+      <Navbar
+        style={{
+          backgroundColor:
+            this.state.status === "top" ? "transparent" : "white",
+          transition: this.state.status === "top" ? "0.3s" : "0.3s",
+          boxShadow:
+            this.state.status === "top"
+              ? "none"
+              : "0px 4px 8px 0px rgba(205, 205, 205, 0.26)",
+        }}
+        expand="md"
+        fixed="top"
+      >
+        {this.state.show ? <img src={Logo} alt="" id="Logo"></img> : null}
+        {screen.availWidth < 768 ? <p>{title}</p> : null}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto ml-auto">
+            <div className="NavLinks">
+              <Nav.Link
+                href="#about-container"
+                style={{
+                  color: this.state.status === "top" ? "white" : "black",
+                }}
+              >
+                About
+              </Nav.Link>
+            </div>
+            <div className="NavLinks">
+              <Nav.Link
+                href="#Speakers"
+                style={{
+                  color: this.state.status === "top" ? "white" : "black",
+                }}
+              >
+                Speakers
+              </Nav.Link>
+            </div>
+            <div className="NavLinks">
+              <Nav.Link
+                href="#Timeline"
+                style={{
+                  color: this.state.status === "top" ? "white" : "black",
+                }}
+              >
+                Timeline
+              </Nav.Link>
+            </div>
+            <div className="NavLinks">
+              <Nav.Link
+                href="#FAQs"
+                style={{
+                  color: this.state.status === "top" ? "white" : "black",
+                }}
+              >
+                FAQs
+              </Nav.Link>
+            </div>
+            <div className="NavLinks">
+              <Nav.Link
+                href="#ContactUs"
+                style={{
+                  color: this.state.status === "top" ? "white" : "black",
+                }}
+              >
+                Contact Us
+              </Nav.Link>
+            </div>
+            {this.state.show ? (
+              <div className="NavLinks">
+                <a
+                  className="nav-link"
+                  href="#Donate"
+                  style={{ color: "#4329E8" }}
+                >
+                  Donate now
+                </a>
+              </div>
+            ) : null}
+          </Nav>
+          {screen.availWidth < 768 ? (
+            <div className="NavLinks  Reg">
+              <a href="#Register">Register Now</a>
+            </div>
+          ) : (
+            <Button
+              variant="outlined"
+              style={{
+                color: this.state.status === "top" ? "white" : "#4329E8",
+                borderColor: this.state.status === "top" ? "white" : "#4329E8",
+              }}
+            >
+              REGISTER NOW
+            </Button>
+          )}
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
 }
--- src/screens/LandingScreen/LandingScreen.jsx
+++ src/screens/LandingScreen/LandingScreen.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./LandingScreen.css";
-import Navbar from "../../components/LandingPage/Navbar/Navbar";
+import NavbarComponent from "../../components/LandingPage/Navbar/Navbar";
 import Hero from "../../components/LandingPage/Hero/Hero";
 import About from "../../components/LandingPage/About/About";
 import { ThemeProvider, createMuiTheme } from "@material-ui/core";
@@ -15,7 +15,7 @@ export default function LandingScreen() {
   return (
     <div>
       <ThemeProvider theme={theme}>
-        <Navbar />
+        <NavbarComponent />
         <Hero />
         <About />
       </ThemeProvider>

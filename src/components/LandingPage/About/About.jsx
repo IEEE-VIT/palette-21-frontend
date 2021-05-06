845b7360c64cac7a23124fd6f74ccc8079426fb9
Crazy-C0der <sh2000.cool.j@gmail.com>
2021-05-06 12:07:01 +0530

    about section corrected

--- src/components/LandingPage/About/About.jsx
+++ src/components/LandingPage/About/About.jsx
@@ -1,28 +1,43 @@
 import React from "react";
-import Typography from "@material-ui/core/Typography";
+//import Typography from "@material-ui/core/Typography";
 import Palette from "../../../assets/Palette.svg";
+import dots from "../../../assets/dots-bkgd.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Row, Col } from "react-bootstrap";
 
 import "./About.css";
+import { Typography } from "@material-ui/core";
 
 export default function About() {
   return (
     <div>
-      <div className="content">
+      <img src={dots} alt="" className="dots"></img>
+      <div className="content1">
         <Typography variant="h2" id="about">
           About
         </Typography>
-        <div className="flex-container">
-          <div className="flex-item" id="description">
-            <Typography>
-              While the last two editions witnessed amazing graphic designs and
-              web designs, this year it is a 36 hour long design hack based
-              entirely on the concepts of UI/UX.
+        <Row>
+          <Col className="cont" xs={12} md={6}>
+            <Typography id="cont">
+              Palette ‘21 is the 4th edition of the prestigious design-a-thon
+              conducted yearly by IEEE VIT. While the last three editions
+              witnessed amazing graphic designs and web designs, this year it is
+              a 48 hour long design hack based entirely on the concepts of UI &
+              UX. We strongly believe in the saying “Design is not how it looks,
+              design is how it works”, we will be looking at not just the output
+              or presentation but the design process and steps involved like
+              research, building user personas, a design system, wireframing,
+              branding and prototypying. Once the event goes live, the
+              participants will be given a set of randomly generated UX problems
+              and they will be continuing with one of their choice.
             </Typography>
-          </div>
-          <div className="flex-item" id="logo">
-            <img src={Palette} alt="Palette" id="paletteLogo"></img>
-          </div>
-        </div>
+          </Col>
+          <Col xs={12} md={6}>
+            <div id="logo">
+              <img src={Palette} alt="Palette" id="paletteLogo"></img>
+            </div>
+          </Col>
+        </Row>
       </div>
       <div className="marquee">
         <div className="container r1">

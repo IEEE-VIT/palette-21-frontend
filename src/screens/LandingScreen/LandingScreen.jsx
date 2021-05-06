b6cfae5e9b2fc7b4595f95dae270739d65af1647
Crazy-C0der <sh2000.cool.j@gmail.com>
2021-05-06 12:10:22 +0530

    material wrapper-font

--- src/screens/LandingScreen/LandingScreen.jsx
+++ src/screens/LandingScreen/LandingScreen.jsx
@@ -3,13 +3,22 @@ import "./LandingScreen.css";
 import Navbar from "../../components/LandingPage/Navbar/Navbar";
 import Hero from "../../components/LandingPage/Hero/Hero";
 import About from "../../components/LandingPage/About/About";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: ["Neue Montreal"].join(","),
+  },
+});
 
 export default function LandingScreen() {
   return (
     <div>
-      <Navbar />
-      <Hero />
-      <About />
+      <ThemeProvider theme={theme}>
+        <Navbar />
+        <Hero />
+        <About />
+      </ThemeProvider>
     </div>
   );
 }

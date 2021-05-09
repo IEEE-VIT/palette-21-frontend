8636d882e4dbbdb32f71f35cad76ae2502801b56
Agniva Basak <agnivabasak1@gmail.com>
2021-05-09 14:42:07 +0530

    changed some brand stuff

--- src/components/LandingPage/Brand/Brand.jsx
+++ src/components/LandingPage/Brand/Brand.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import './Brand.css';
 import brandimg from "../../../images/brand-img-desktop.png";
-import brandimg_mobile from "../../../images/brand-img.png"
 
 const Brand = () => {
     return(
@@ -12,7 +11,6 @@ const Brand = () => {
                         <p>The one of its kind Design Hack</p>
                     </div>
                     <img src={brandimg} id="brand-img" />
-                    <img src={brandimg_mobile} id="brand-img-mobile" /> 
             </div>
     );
   

e1deaccef10c6b28bf66282f742785a1d6d36fe4
Agniva Basak <agnivabasak1@gmail.com>
2021-05-12 22:27:37 +0530

    minor refactor

--- src/screens/LandingScreen/LandingScreen.jsx
+++ src/screens/LandingScreen/LandingScreen.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./LandingScreen.css";
 
-import Navbar from "../../components/LandingPage/Navbar/Navbar";
-import NavbarComponent from "../../components/LandingPage/Navbar/Navbar";
-import Hero from "../../components/LandingPage/Hero/Hero";
-import About from "../../components/LandingPage/About/About";
-import EventThemes from "../../components/LandingPage/EventThemes/EventThemes";
-import EventCarousel from "../../components/LandingPage/Event/EventCarouselSection";
-import TestimonialCarousel from "../../components/LandingPage/Testimonials/TestimonialCarousel";
-import Footer from "../../components/LandingPage/Footer/Footer";
+import Navbar from "../../components/LandingScreen/Navbar/Navbar";
+import NavbarComponent from "../../components/LandingScreen/Navbar/Navbar";
+import Hero from "../../components/LandingScreen/Hero/Hero";
+import About from "../../components/LandingScreen/About/About";
+import EventThemes from "../../components/LandingScreen/EventThemes/EventThemes";
+import EventCarousel from "../../components/LandingScreen/Event/EventCarouselSection";
+import TestimonialCarousel from "../../components/LandingScreen/Testimonials/TestimonialCarousel";
+import Footer from "../../components/LandingScreen/Footer/Footer";
 import { ThemeProvider, createMuiTheme } from "@material-ui/core";
 
 const theme = createMuiTheme({

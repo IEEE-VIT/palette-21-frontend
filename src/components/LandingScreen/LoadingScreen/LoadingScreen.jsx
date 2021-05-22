64f9441ccc26671d912e45fad57816f52a407eca
r-ush <aarush.bhatt@gmail.com>
2021-05-22 13:52:57 +0530

    fix: minor css fix

---
+++ src/components/LandingScreen/LoadingScreen/LoadingScreen.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Loading.css";
+
+const LoadingScreen = () => {
+  return (
+    <div>
+      <div>Loader</div>
+    </div>
+  );
+};
+
+export default LoadingScreen;

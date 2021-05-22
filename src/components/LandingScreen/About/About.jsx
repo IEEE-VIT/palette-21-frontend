25065fd4e7c1816ce0f4298b09d98e561846e000
DarthBenro008 <hkpdev008@gmail.com>
2021-05-22 12:14:30 +0530

    fix: landing page

--- src/components/LandingScreen/About/About.jsx
+++ src/components/LandingScreen/About/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 //import Typography from "@material-ui/core/Typography";
-import Palette from "../../../assets/Palette.svg";
+import Palette from "../../../assets/palette_logo.png";
 import dots from "../../../assets/dots-bkgd.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Row, Col } from "react-bootstrap";
--- src/components/LandingScreen/Event/EventData.js
+++ src/components/LandingScreen/Event/EventData.js
@@ -1,5 +1,4 @@
 import speakerEv1 from "../../../assets/speaker-ev-1.png";
-import speakerEv2 from "../../../assets/speaker-ev-2.png";
 import speakerEv3 from "../../../assets/speaker-ev-3.png";
 
 const eventData = [
@@ -8,15 +7,6 @@ const eventData = [
     speakerName: "Parinishtha",
     speakerDesc: "UX designer- II, Microsoft",
     speakerPic: speakerEv1,
-    eventName: "Designing for the next billion generation",
-    eventCategory: "DESIGN TALKS",
-    eventDesc: "",
-  },
-  {
-    date: "28th May 2021",
-    speakerName: "Aditi Sharma",
-    speakerDesc: "Product Designer at Walmart Labs",
-    speakerPic: speakerEv2,
     eventName: "Designing for the next billion generation",
     eventCategory: "DESIGN TALKS",
     eventDesc: "",

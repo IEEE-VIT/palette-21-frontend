6d88f4b7bada9b8231d45e16d51c254df1c9cf5d
r-ush <aarush.bhatt@gmail.com>
2021-05-23 13:01:11 +0530

    add: locked ps gen

--- src/components/ProblemStatement/ProblemStatement.jsx
+++ src/components/ProblemStatement/ProblemStatement.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import "./ProblemStatement.css";
 import PropTypes from "prop-types";
+import cookies from "react-cookies";
 
+import psLockedDark from "../../assets/pslockeddark.png";
+import psLockedLight from "../../assets/pslockedlight.png";
 export default function ProblemStatement({ mode, selectedPage }) {
   console.log(mode, selectedPage);
+  var curMode = cookies.load("mode");
   return (
     <div
       id="ProblemStatement__container"
       style={{ display: selectedPage == 5 ? "flex" : "none" }}
     >
-      <h1> Make an app for vit students to fight depression</h1>
+      {curMode === "dark" ? (
+        <img className="non-drag-img" src={psLockedDark} alt="" />
+      ) : (
+        <img className="non-drag-img" src={psLockedLight} alt="" />
+      )}
     </div>
   );
 }

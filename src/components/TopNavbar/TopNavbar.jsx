e6106f9ef40ad1f8b599e90200b0fc08d8da3a61
Agniva Basak <agnivabasak1@gmail.com>
2021-05-23 14:14:53 +0530

    added logout feature lezzgo

--- src/components/TopNavbar/TopNavbar.jsx
+++ src/components/TopNavbar/TopNavbar.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TopNavbar.css";
 import PropTypes from "prop-types";
 import ModeSlider from "../ModeSlider/ModeSlider";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import cookies from "react-cookies";
 
 export default function TopNavbar(props) {
+  const [anchorEl, setAnchorEl] = useState(false);
+
+  const handleClose = () => {
+    setAnchorEl(false);
+  };
   return (
     <div
       id="TopNavbar__container"
@@ -16,8 +24,36 @@ export default function TopNavbar(props) {
       <div id="TopNavbar__profile">
         <ModeSlider mode={props.mode} setMode={(m) => props.setMode(m)} />
         <h2>Hey, {props.name}</h2>
-        <img src={props.imgUrl} />
+        <img
+          src={props.imgUrl}
+          onClick={() => {
+            setAnchorEl(!anchorEl);
+          }}
+        />
       </div>
+      <Menu
+        id="simple-menu"
+        className="TopNavbar__logout"
+        anchorEl={anchorEl}
+        keepMounted
+        getContentAnchorEl={null}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
+        open={anchorEl}
+        onClose={handleClose}
+      >
+        <MenuItem
+          onClick={() => {
+            handleClose();
+            cookies.remove("token");
+            cookies.remove("mode");
+            localStorage.clear();
+            window.location.reload();
+          }}
+        >
+          <i className="fas fa-sign-out-alt"></i> Logout
+        </MenuItem>
+      </Menu>
     </div>
   );
 }
